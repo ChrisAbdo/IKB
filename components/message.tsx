d71d995bd94c77be3663750379aee6e3378985ab
Chris Abdo <chris.j.abdo@gmail.com>
2024-09-22 03:19:45 -0400

    .

--- components/message.tsx
+++ components/message.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { BotIcon, UserIcon } from "./icons";
 import { ReactNode } from "react";
 import { Markdown } from "./markdown";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
